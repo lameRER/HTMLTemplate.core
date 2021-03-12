817a3e4ffae69d58cdb22383a3420e109966d4e7
lameR <erpresents@gmail.com>
2021-03-12 03:14:55 +0300

    Modify SqlTools.cs

--- SqlTools.cs
+++ SqlTools.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -9,17 +10,31 @@ namespace HTMLTemplate
     [Serializable]
     public class SqlTools
     {
+        private List<SqlConnect> _connections;
 
         [JsonPropertyName("sqltools.connections")]
-        public List<Connect> Connections { get; set; }
+        public List<SqlConnect> Connections
+        {
+            get => _connections;
+            set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(value));
+                _connections = value ?? throw new ArgumentNullException(nameof(value));
+            }
+        }
 
         public SqlTools()
         {
-            
+            var eventclass = new EventClass();
+            try
+            {
+                eventclass.Select(MethodBase.GetCurrentMethod()?.ReflectedType?.Name);
+            }
+            catch (Exception e)
+            {
+                eventclass.Error(MethodBase.GetCurrentMethod()?.ReflectedType?.Name, e);
+            }
         }
-        public static SqlTools SqlToolsDeserialize(string getSettingsFile)
-        {
-            return JsonSerializer.Deserialize<SqlTools>(getSettingsFile);
-        }
+        public static SqlTools SqlToolsDeserialize(string getSettingsFile) => JsonSerializer.Deserialize<SqlTools>(getSettingsFile);
     }
 }
