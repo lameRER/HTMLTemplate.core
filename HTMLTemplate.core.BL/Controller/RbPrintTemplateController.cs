8b9c94e65162f4f5db28a65d35d86880aff9bde8
lameR <erpresents@gmail.com>
2021-05-01 02:49:40 +0300

    Create class RbPrintTemplateController

---
+++ HTMLTemplate.core.BL/Controller/RbPrintTemplateController.cs
@@ -0,0 +1,9 @@
+using HTMLTemplate.core.BL.Base;
+
+namespace HTMLTemplate.core.BL.Controller
+{
+    public class RbPrintTemplateController : BaseRbPrintTemplateController
+    {
+        public override BaseRbPrintTemplateController PrintTemplate { get; }
+    }
+}
