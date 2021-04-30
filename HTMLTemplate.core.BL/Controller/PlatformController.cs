6b7edd02736f83eb49dce8e8573e7591e177a07a
lameR <erpresents@gmail.com>
2021-04-30 22:02:26 +0300

    Modify PlatformController

--- HTMLTemplate.core.BL/Controller/PlatformController.cs
+++ HTMLTemplate.core.BL/Controller/PlatformController.cs
@@ -1,15 +1,15 @@
 using HTMLTemplate.core.BL.Base;
-using HTMLTemplate.core.BL.InterfaceController;
 using HTMLTemplate.core.BL.Model;
 
 namespace HTMLTemplate.core.BL.Controller
 {
     public class PlatformController : BasePlatformController
     {
-        public sealed override Platform Platform { get; } = new();
+        public override Platform Platform { get; } = new();
 
-        public PlatformController(): base()
+        public PlatformController() : base()
         {
+            
         }
     }
 }
