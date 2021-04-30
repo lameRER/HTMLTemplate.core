3cca59f9a0b323874922e4ef878a898d0b0ff8af
lameR <erpresents@gmail.com>
2021-05-01 02:49:52 +0300

    Create class TemplateCreate

---
+++ HTMLTemplate.core.BL/Controller/TemplateCreate.cs
@@ -0,0 +1,12 @@
+using HTMLTemplate.core.BL.Model;
+
+namespace HTMLTemplate.core.BL.Controller
+{
+    public class TemplateCreate
+    {
+        public TemplateCreate()
+        {
+            var file = new TemplateFile();
+        }
+    }
+}
