9474e8565f056c7d0a3374a805af31d1790c3aa4
lameR <erpresents@gmail.com>
2021-05-01 00:32:23 +0300

    Create BaseSetting

---
+++ HTMLTemplate.core.BL/Base/BaseSetting.cs
@@ -0,0 +1,11 @@
+using System;
+using System.IO;
+using HTMLTemplate.core.BL.Model;
+
+namespace HTMLTemplate.core.BL.Base
+{
+    public abstract class BaseSetting
+    {
+        public abstract string GetSettingsFile();
+    }
+}
