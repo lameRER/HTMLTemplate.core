0226f31769863eb06b0b65e63f36a3151650b210
lameR <erpresents@gmail.com>
2021-04-30 22:02:00 +0300

    Delete

--- HTMLTemplate.core.BL/Interface/IPlatform.cs
+++ HTMLTemplate.core.BL/Interface/IPlatform.cs
@@ -1,8 +1,8 @@
 using System;
 
-namespace HTMLTemplate.core.BL.InterfaceModel
+namespace HTMLTemplate.core.BL.Interface
 {
-    internal interface IPlatformProperty
+    internal interface IPlatform
     {
         public abstract PlatformID PlatformId { get; internal set; }
         public abstract string UserName { get; internal set; }
