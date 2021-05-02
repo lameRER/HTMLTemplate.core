00fe7fc942c3dbb7a2f43f118564ab0dd70a43fb
lameR <erpresents@gmail.com>
2021-05-02 04:24:38 +0300

    Delete class SettingController

--- HTMLTemplate.core.BL/Controller/RbPrintTemplateController.cs
+++
@@ -1,9 +0,0 @@
-using HTMLTemplate.core.BL.Base;
-
-namespace HTMLTemplate.core.BL.Controller
-{
-    public class RbPrintTemplateController : BaseRbPrintTemplateController
-    {
-        public override BaseRbPrintTemplateController PrintTemplate { get; }
-    }
-}
