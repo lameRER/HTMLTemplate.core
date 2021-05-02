019de62280d086dab63b8e43601e99ddec4b6e7e
lameR <erpresents@gmail.com>
2021-05-02 04:22:18 +0300

    Delete Base class BaseRbPrintTemplateController

--- HTMLTemplate.core.BL/Base/BaseRbPrintTemplateController.cs
+++
@@ -1,7 +0,0 @@
-namespace HTMLTemplate.core.BL.Base
-{
-    public abstract class BaseRbPrintTemplateController 
-    {
-        public abstract BaseRbPrintTemplateController PrintTemplate{get;}
-    }
-}
