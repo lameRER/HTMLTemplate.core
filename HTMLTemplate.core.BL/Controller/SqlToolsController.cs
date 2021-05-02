eacd808527d715c318afd2346c1542d98a036647
lameR <erpresents@gmail.com>
2021-05-02 04:25:06 +0300

    Modify class SqlConnectController

--- HTMLTemplate.core.BL/Controller/SqlToolsController.cs
+++ HTMLTemplate.core.BL/Controller/SqlToolsController.cs
@@ -1,25 +1,31 @@
+#nullable enable
 using System;
-using System.Collections.Generic;
+using System.Reflection;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using HTMLTemplate.core.BL.Base;
-using HTMLTemplate.core.BL.Interface;
 using HTMLTemplate.core.BL.Model;
 
 namespace HTMLTemplate.core.BL.Controller
 {
-    public class SqlToolsController : BaseSqlToolsController
+    public class SqlConnectController
     {
-        public override SettingSqlToolsConnects SqlToolsConnects { get; set; }
+        public ListSqlConnects? SqlToolsConnects { get; } = new();
 
-        public SqlToolsController()
+        public SqlConnectController(string getSettingsFile)
         {
-            
+            var eventNotify = new EventNotify();
+            try
+            {
+                SqlToolsConnects = SqlToolsDeserialize(getSettingsFile);
+            }
+            catch (Exception e)
+            {
+                eventNotify.Error(MethodBase.GetCurrentMethod()?.ReflectedType?.Name, e);
+            }
         }
-        
-        public override SettingSqlToolsConnects SqlToolsDeserialize(string getSettingsFile)
+
+        private static ListSqlConnects? SqlToolsDeserialize(string getSettingsFile)
         {
-            return JsonSerializer.Deserialize<SettingSqlToolsConnects>(getSettingsFile);   
+            return JsonSerializer.Deserialize<ListSqlConnects>(getSettingsFile);
         }
     }
 }
