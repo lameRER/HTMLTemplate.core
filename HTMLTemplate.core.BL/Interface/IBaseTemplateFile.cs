e930396b9842ebf5a074174afb129dd69c2e11d9
lameR <erpresents@gmail.com>
2021-05-02 04:36:01 +0300

    Create interface
    IBaseTemplateFile

---
+++ HTMLTemplate.core.BL/Interface/IBaseTemplateFile.cs
@@ -0,0 +1,9 @@
+using HTMLTemplate.core.BL.Model;
+
+namespace HTMLTemplate.core.BL.Interface
+{
+    public interface IBaseTemplateFile
+    {
+        public TemplateFile TemplateFile { get; set; }
+    }
+}
