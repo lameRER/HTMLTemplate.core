195300ae548c0f15d68106e0daf0f743c1c843a3
lameR <erpresents@gmail.com>
2021-06-01 04:06:33 +0300

    Feature

--- HTMLTemplate.core.BL/Controller/MysqlController.cs
+++ HTMLTemplate.core.BL/Controller/MysqlController.cs
@@ -3,20 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using HTMLTemplate.core.BL.Model;
+using HTMLTemplate.core.BL.Models;
 
 namespace HTMLTemplate.core.BL.Controller
 {
     public class MysqlController 
     {
         private readonly SqlConnectProperty _connect;
-        private readonly string? _fileName;
-        private readonly string? _fileCode;
 
-        public MysqlController(SqlConnectProperty? connect, string? fileName, string? fileCode)
+        public MysqlController(SqlConnectProperty? connect)
         {
             _connect = connect ?? throw new ArgumentNullException(nameof(connect));
-            _fileName = fileName ?? throw new ArgumentNullException(nameof(fileName));
-            _fileCode = fileCode ?? throw new ArgumentNullException(nameof(fileCode));
         }
 
         public DataBaseContext MySqlConnect()
@@ -24,9 +21,13 @@ namespace HTMLTemplate.core.BL.Controller
             return new(_connect);
         }
         
-        public IEnumerable<ActionType> GetValue(DataBaseContext context)
+        public IEnumerable<RbPrintTemplate> GetRbPrintTemplateValue(DataBaseContext context)
         {
-            return context.ActionTypes.Where(e => e.Name == _fileName &&  e.Code == _fileCode);
+            return context.RbPrintTemplates.ToList();
+        }
+        public IEnumerable<ActionType> GetActionTypeValue(DataBaseContext context, string? fileName, string? fileCode)
+        {
+            return context.ActionTypes.Where(e => e.Name == fileName &&  e.Code == fileCode);
         }
     }
 }
