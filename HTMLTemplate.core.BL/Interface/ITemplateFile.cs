d67cce2ec6169af40ad1de4f905c1f0709dd317a
lameR <erpresents@gmail.com>
2021-09-04 00:49:25 +0300

    Fix 7 Style, 1 Maintainability issues in multiple files

--- HTMLTemplate.core.BL/Interface/ITemplateFile.cs
+++ HTMLTemplate.core.BL/Interface/ITemplateFile.cs
@@ -4,10 +4,10 @@ namespace HTMLTemplate.core.BL.Interface
 {
     public interface ITemplateFile
     {
-        public string DirectoryFile {get; set;}
-        public string DirectoryTemplate {get; set;}
-        public string FileName {get; set;}
-        public string FileCode {get; set;}
+        public string DirectoryFile { get; set; }
+        public string DirectoryTemplate { get; set; }
+        public string FileName { get; set; }
+        public string FileCode { get; set; }
         public List<string> TemplateLine { get; set; }
     }
 }
--- HTMLTemplate.core.Tests/Tets.cs
+++ HTMLTemplate.core.Tests/Tets.cs
@@ -8,10 +8,7 @@ namespace HTMLTemplate.core.Tests
     public class Tests
     {
         [SetUp]
-        public void Setup()
-        {
-            
-        }
+        public void Setup(){ }
 
         [Test]
         public void MysqlStatic()
